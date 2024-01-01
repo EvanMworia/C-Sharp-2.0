6136dcd207ca64c55d6129c7df283471279d0c57
EvanMworia <evannji99@gmail.com>
2024-01-01 23:54:53 +0300

    Added Services to Product MS

---
+++ 0x004 - MicroServices/E-CommerceMS/ProductService/Services/ProductSVC.cs
@@ -0,0 +1,34 @@
+﻿using ProductService.Models;
+using ProductService.Models.DTOs;
+using ProductService.Services.IServices;
+
+namespace ProductService.Services
+{
+    public class ProductSVC : IProduct
+    {
+        Task<string> IProduct.AddNewProduct(Product newProduct)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<string> IProduct.DeleteProduct(ProductDTO product)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<Product> IProduct.GetProductById(Guid ProductId)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<ProductDTO> IProduct.SearchForProductByName(GetProductDTO productName)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<string> IProduct.UpdateProduct()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
