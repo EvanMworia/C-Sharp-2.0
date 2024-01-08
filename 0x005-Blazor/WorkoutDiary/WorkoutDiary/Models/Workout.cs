2b648880db5506d589970601e6e095d4ce7a22fe
EvanMworia <evannji99@gmail.com>
2024-01-08 15:37:17 +0300

    Listed Workout days on cards

---
+++ 0x005-Blazor/WorkoutDiary/WorkoutDiary/Models/Workout.cs
@@ -0,0 +1,10 @@
+﻿namespace WorkoutDiary.Models
+{
+    public class Workout
+    {
+        public string Name { get; set; }=string.Empty;
+        public string TargetArea { get; set; } = string.Empty;
+
+
+    }
+}
