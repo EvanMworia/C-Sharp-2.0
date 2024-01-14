dc40bf0935f9a8ce918673ec3439e9f4d2ae938a
EvanMworia <evannji99@gmail.com>
2024-01-14 18:47:30 +0300

    Publishing and adding messages to our queue

---
+++ 0x004 - MicroServices/E-CommerceMS/Auth_Service/Models/DTOs/UserMessageDTO.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Auth_Service.Models.DTOs
+{
+    public class UserMessageDTO
+    {
+        public string UserName { get; set; } = string.Empty;
+        public string EmailAddress { get; set; } = string.Empty;
+    }
+}
