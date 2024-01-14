e1c31940763f1b98dc2c44d11fbb8b0993baf3bd
EvanMworia <evannji99@gmail.com>
2024-01-14 18:44:37 +0300

    Migrations added

---
+++ 0x004 - MicroServices/BlogMS/Comments/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Comments.Model;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace Comments.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Comment> Comments { get; set; }
+        
+    }
+}
