61e0278ed77325f3a4b5aeacd4e2d56924c1335b
EvanMworia <evannji99@gmail.com>
2024-01-19 16:30:51 +0300

    Added Login and Register

--- 0x006-FinalProject(ArtGallery AuctionSystem)/ArtGallery-AuctionSystem/ArtGalleryFrontend/Models/Auth-Models/LoginRequestDTO.cs
+++ 0x006-FinalProject(ArtGallery AuctionSystem)/ArtGallery-AuctionSystem/ArtGalleryFrontend/Models/Auth-Models/LoginRequestDTO.cs
@@ -2,15 +2,11 @@
 
 namespace ArtGalleryFrontend.Models.Auth_Models
 {
-    public class UserDTO
+    public class LoginRequestDTO
     {
-        public Guid Id { get; set; }
-        public string FullNames { get; set; } = string.Empty;
         [Required]
         public string UserName { get; set; } = string.Empty;
-
-        public string Email { get; set; } = string.Empty;
-
-        
+        [Required]
+        public string Password { get; set; } = string.Empty;
     }
 }
